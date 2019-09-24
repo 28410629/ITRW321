30cbea9abb0e3fa3e89dd07346a757f0d0bbb863
Coenraad Human <coen.human@gmail.com>
2019-09-24 18:55:32 +0200

    Getting there fact_reading

--- Work_Coenraad/fact_reading.sql
+++ Work_Coenraad/fact_reading.sql
@@ -1,31 +1,44 @@
-INSERT INTO FACT_READING
-(
-SELECT S.STATION_ID, S.LOCATION_ID, 
-    /* Create time id for use for insert */
-    SYSDATE
-, (
-    /* Nested query for air pressure for a station id. */
-    SELECT AVG(AIR_PRESSURE)
-    FROM STATIONREADING
-    WHERE STATIONID = S.STATIONID
-), (
-    /* Nested query for ambient light for a station id. */
-    SELECT AVG(AIR_PRESSURE)
-    FROM STATIONREADING
-    WHERE STATIONID = S.STATIONID
-), (
-    /* Nested query for humdity for a station id. */
-    SELECT AVG(AIR_PRESSURE)
-    FROM STATIONREADING
-    WHERE STATIONID = S.STATIONID
-), (
-    /* Nested query for temperature for a station id. */
-    SELECT AVG(AIR_PRESSURE)
-    FROM STATIONREADING
-    WHERE STATIONID = S.STATIONID
-)
-FROM STATIONREADING R
-JOIN STATION S
-ON S.STATIONID = R.STATIONID
-GROUP BY S.STATIONID, S.STATION_ID, S.LOCATION_ID, SYSDATE
-);
+DECLARE
+    l_today_date DATE := SYSDATE;
+    l_today_timestamp TIMESTAMP := SYSTIMESTAMP;
+BEGIN
+    time := SYSDATE;
+    INSERT INTO DIM_TIME (time_id, hour, day, week, month, year)
+    VALUES    
+    (
+        l_today_date, 
+        EXTRACT (hour FROM l_today_timestamp),
+        EXTRACT (day FROM l_today_date),
+        EXTRACT (month FROM l_today_date),
+        EXTRACT (month FROM l_today_date),
+        EXTRACT (year FROM l_today_date)
+    );
+    INSERT INTO FACT_READING
+    (
+        SELECT s.stationid, s.locationid, l_today_date, (
+            /* Nested query for air pressure for a station id. */
+            SELECT AVG(stationreading.air_pressure)
+            FROM stationreading
+            WHERE stationid = s.stationid
+        ), (
+            /* Nested query for ambient light for a station id. */
+            SELECT AVG(stationreading.ambient_light)
+            FROM stationreading
+            WHERE stationid = s.stationid
+        ), (
+            /* Nested query for humdity for a station id. */
+            SELECT AVG(stationreading.humidity)
+            FROM stationreading
+            WHERE stationid = s.stationid
+        ), (
+            /* Nested query for temperature for a station id. */
+            SELECT AVG(stationreading.temperature)
+            FROM stationreading
+            WHERE stationid = s.stationid
+    )
+    FROM stationreading R
+    JOIN station S
+    ON s.stationid = r.stationid
+    GROUP BY s.stationid, s.locationid, l_today_date
+    );
+END;

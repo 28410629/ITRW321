0961ecf229615c21c718f5d3c32c86fa363558a0
Coenraad Human <coen.human@gmail.com>
2019-09-26 10:32:30 +0200

    Added dotnet console application for getting readings from api.

---
+++ GetDataFromAPI-GenerateSQL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetDataFromAPI_GenerateSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

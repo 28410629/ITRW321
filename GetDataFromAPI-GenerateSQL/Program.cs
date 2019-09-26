946a82dff18404e2cda2a7e40cf27700d30cf646
Coenraad Human <coen.human@gmail.com>
2019-09-26 13:19:46 +0200

    Added error tracking for program

--- GetDataFromAPI-GenerateSQL/Program.cs
+++ GetDataFromAPI-GenerateSQL/Program.cs
@@ -10,8 +10,11 @@ namespace GetDataFromAPI_GenerateSQL
         static void Main(string[] args)
         {
             // Get objects
+            Console.WriteLine("[ OK! ] Getting json for station 2347795");
             var station0 = GetJsonObject(@"http://weatherstationapi.ddns.net:5000/api/get/rawreadings/station/day?StationId=2347795");
+            Console.WriteLine("[ OK! ] Getting json for station 2347795");
             var station1 = GetJsonObject(@"http://weatherstationapi.ddns.net:5000/api/get/rawreadings/station/day?StationId=10359807");
+            Console.WriteLine("[ OK! ] Getting json for station 2347795");
             var station2 = GetJsonObject(@"http://weatherstationapi.ddns.net:5000/api/get/rawreadings/station/day?StationId=10359964");
             
             // Get statistics on objects
@@ -22,37 +25,56 @@ namespace GetDataFromAPI_GenerateSQL
             // Transform data on usable objects
             if (station0Length != 0)
             {
-                Console.WriteLine("2347795: " + station0Length);
+                Console.WriteLine("[ OK! ] Starting to transform station 2347795");
+                GenerateInserSql(station0);
             }
             if (station1Length != 0)
             {
-                Console.WriteLine("10359807: " + station1Length);
+                Console.WriteLine("[ OK! ] Starting to transform station 10359807");
+                GenerateInserSql(station1);
             }
             if (station2Length != 0)
             {
-                Console.WriteLine("10359964: " + station2Length);
+                Console.WriteLine("[ OK! ] Starting to transform station 10359964");
+                GenerateInserSql(station2);
             }
         }
 
         static RawReadingsDto GetJsonObject(string url)
         {
-            var webRequest = WebRequest.Create(url) as HttpWebRequest;
-            if (webRequest == null)
+            try
             {
-                return null;
-            }
-
-            webRequest.ContentType = "application/json";
-            webRequest.UserAgent = "Nothing";
-
-            using (var s = webRequest.GetResponse().GetResponseStream())
-            {
-                using (var sr = new StreamReader(s))
+                Console.WriteLine("[ OK! ] Creating HttpWebRequest");
+                var webRequest = WebRequest.Create(url) as HttpWebRequest;
+                if (webRequest == null)
                 {
-                    var reponse = sr.ReadToEnd();
-                    return JsonConvert.DeserializeObject<RawReadingsDto>(reponse);
+                    Console.WriteLine("[ ERR ] HttpWebRequest return null");
+                    return null;
+                }
+
+                webRequest.ContentType = "application/json";
+                webRequest.UserAgent = "Nothing";
+
+                using (var s = webRequest.GetResponse().GetResponseStream())
+                {
+                    using (var sr = new StreamReader(s))
+                    {
+                        Console.WriteLine("[ OK! ] Creating object from json");
+                        var reponse = sr.ReadToEnd();
+                        return JsonConvert.DeserializeObject<RawReadingsDto>(reponse);
+                    }
                 }
             }
+            catch (Exception e)
+            {
+                Console.WriteLine("[ ERR ] " + e.Message);
+            }
+            
+        }
+
+        static Void GenerateInserSql(RawReadingsDto json)
+        {
+            
         }
     }
 }

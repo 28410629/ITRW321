28bf2852d1e733b81fbb547cad744466e5b34045
Morne <morneventer.mv@gmail.com>
2019-09-30 13:33:27 +0200

    FIXED TIME TABLE

--- Final_Database_Project/04-datapoints_for_business.sql
+++ Final_Database_Project/04-datapoints_for_business.sql
@@ -145,7 +145,7 @@ GROUP BY E.POSITION_TYPE;
 
 --Avergae humidity per location for ALL TIME
 SELECT l.location_name "Location Name", ROUND(AVG(f.avg_humidity),2) "Average Humidity"
-FROM fact_reading f 
+FROM fact_reading f
 JOIN dim_location l
 ON l.location_id = f.location_id
 GROUP BY l.location_name;
@@ -153,36 +153,36 @@ GROUP BY l.location_name;
 --Avergae humidity per location for PAST HOUR
 --------------------------------------------------------------------------------
 SELECT l.location_name "Location Name", ROUND(AVG(f.avg_humidity),2) "Average Humidity"
-FROM fact_reading f 
+FROM fact_reading f
 JOIN dim_location l
-ON l.location_id = f.location_id 
+ON l.location_id = f.location_id
 AND f.time_id > (SYSDATE-1/24)
 GROUP BY l.location_name;
 --------------------------------------------------------------------------------
 --Avergae humidity per location for PAST DAY
 --------------------------------------------------------------------------------
 SELECT l.location_name "Location Name", ROUND(AVG(f.avg_humidity),2) "Average Humidity"
-FROM fact_reading f 
+FROM fact_reading f
 JOIN dim_location l
-ON l.location_id = f.location_id 
+ON l.location_id = f.location_id
 AND f.time_id > (SYSDATE-1)
 GROUP BY l.location_name;
 --------------------------------------------------------------------------------
 --Avergae humidity per location for WEEK
 --------------------------------------------------------------------------------
 SELECT l.location_name "Location Name", ROUND(AVG(f.avg_humidity),2) "Average Humidity"
-FROM fact_reading f 
+FROM fact_reading f
 JOIN dim_location l
-ON l.location_id = f.location_id 
+ON l.location_id = f.location_id
 AND f.time_id > (SYSDATE-7)
 GROUP BY l.location_name;
 --------------------------------------------------------------------------------
 --Avergae humidity per location for PAST YEAR
 --------------------------------------------------------------------------------
 SELECT l.location_name "Location Name", ROUND(AVG(f.avg_humidity),2) "Average Humidity"
-FROM fact_reading f 
+FROM fact_reading f
 JOIN dim_location l
-ON l.location_id = f.location_id 
+ON l.location_id = f.location_id
 AND f.time_id > (SYSDATE-365)
 GROUP BY l.location_name;
 --------------------------------------------------------------------------------
@@ -191,40 +191,40 @@ GROUP BY l.location_name;
 
 --Avergae humidity per active station for ALL TIME
 SELECT s.station_id "Station ID", ROUND(AVG(f.avg_humidity),2) "Average Humidity"
-FROM fact_reading f 
+FROM fact_reading f
 JOIN dim_station s
-ON s.station_id = f.station_id 
+ON s.station_id = f.station_id
 AND s.isactive = 1
 GROUP BY s.station_id;
 --------------------------------------------------------------------------------
 --Avergae humidity per active station for PAST HOUR
 --------------------------------------------------------------------------------
 SELECT s.station_id "Station ID", ROUND(AVG(f.avg_humidity),2) "Average Humidity"
-FROM fact_reading f 
+FROM fact_reading f
 JOIN dim_station s
-ON s.station_id = f.station_id 
+ON s.station_id = f.station_id
 AND s.isactive = 1
-AND f.time_id > (SYSDATE-1/24) 
+AND f.time_id > (SYSDATE-1/24)
 GROUP BY s.station_id;
 --------------------------------------------------------------------------------
 --Avergae humidity per active station for PAST DAY
 --------------------------------------------------------------------------------
 SELECT s.station_id "Station ID", ROUND(AVG(f.avg_humidity),2) "Average Humidity"
-FROM fact_reading f 
+FROM fact_reading f
 JOIN dim_station s
-ON s.station_id = f.station_id 
+ON s.station_id = f.station_id
 AND s.isactive = 1
-AND f.time_id > (SYSDATE-1) 
+AND f.time_id > (SYSDATE-1)
 GROUP BY s.station_id;
 --------------------------------------------------------------------------------
 --Avergae humidity per active station for PAST WEEK
 --------------------------------------------------------------------------------
 SELECT s.station_id "Station ID", ROUND(AVG(f.avg_humidity),2) "Average Humidity"
-FROM fact_reading f 
+FROM fact_reading f
 JOIN dim_station s
-ON s.station_id = f.station_id 
+ON s.station_id = f.station_id
 AND s.isactive = 1
-AND f.time_id > (SYSDATE-7) 
+AND f.time_id > (SYSDATE-7)
 GROUP BY s.station_id;
 --------------------------------------------------------------------------------
 --Avergae humidity per active station for PAST YEAR
@@ -232,9 +232,9 @@ GROUP BY s.station_id;
 SELECT s.station_id "Station ID", ROUND(AVG(f.avg_humidity),2) "Average Humidity"
 FROM fact_reading f
 JOIN dim_station s
-ON s.station_id = f.station_id 
+ON s.station_id = f.station_id
 AND s.isactive = 1
-AND f.time_id > (SYSDATE-365) 
+AND f.time_id > (SYSDATE-365)
 GROUP BY s.station_id;
 
 --------------------------------------------------------------------------------
@@ -243,7 +243,7 @@ GROUP BY s.station_id;
 
 --Average ambientlight per location for ALL TIME
 SELECT l.location_name "Location Name", ROUND(AVG(f.avg_ambientlight),2) "Average Ambient Light"
-FROM fact_reading f 
+FROM fact_reading f
 JOIN dim_location l
 ON l.location_id = f.location_id
 GROUP BY l.location_name;
@@ -251,36 +251,36 @@ GROUP BY l.location_name;
 --Average ambientlight per location for PAST HOUR
 --------------------------------------------------------------------------------
 SELECT l.location_name "Location Name", ROUND(AVG(f.avg_ambientlight),2) "Average Ambient Light"
-FROM fact_reading f 
+FROM fact_reading f
 JOIN dim_location l
-ON l.location_id = f.location_id 
+ON l.location_id = f.location_id
 AND f.time_id > (SYSDATE-1/24)
 GROUP BY l.location_name;
 --------------------------------------------------------------------------------
 --Average humidity per location for PAST DAY
 --------------------------------------------------------------------------------
 SELECT l.location_name "Location Name", ROUND(AVG(f.avg_ambientlight),2) "Average Ambient Light"
-FROM fact_reading f 
+FROM fact_reading f
 JOIN dim_location l
-ON l.location_id = f.location_id 
+ON l.location_id = f.location_id
 AND f.time_id > (SYSDATE-1)
 GROUP BY l.location_name;
 --------------------------------------------------------------------------------
 --Average humidity per location for WEEK
 --------------------------------------------------------------------------------
 SELECT l.location_name "Location Name", ROUND(AVG(f.avg_ambientlight),2) "Average Ambient Light"
-FROM fact_reading f 
+FROM fact_reading f
 JOIN dim_location l
-ON l.location_id = f.location_id 
+ON l.location_id = f.location_id
 AND f.time_id > (SYSDATE-7)
 GROUP BY l.location_name;
 --------------------------------------------------------------------------------
 --Average ambientlight per location for PAST YEAR
 --------------------------------------------------------------------------------
 SELECT l.location_name "Location Name", ROUND(AVG(f.avg_ambientlight),2) "Average Ambient Light"
-FROM fact_reading f 
+FROM fact_reading f
 JOIN dim_location l
-ON l.location_id = f.location_id 
+ON l.location_id = f.location_id
 AND f.time_id > (SYSDATE-365)
 GROUP BY l.location_name;
 --------------------------------------------------------------------------------
@@ -289,40 +289,40 @@ GROUP BY l.location_name;
 
 --Avergae ambientlight per active station for ALL TIME
 SELECT s.station_id "Station ID", ROUND(AVG(f.avg_ambientlight),2) "Average Ambient Light"
-FROM fact_reading f 
+FROM fact_reading f
 JOIN dim_station s
-ON s.station_id = f.station_id 
+ON s.station_id = f.station_id
 AND s.isactive = 1
 GROUP BY s.station_id;
 --------------------------------------------------------------------------------
 --Avergae humidity per active station for PAST HOUR
 --------------------------------------------------------------------------------
 SELECT s.station_id "Station ID", ROUND(AVG(f.avg_ambientlight),2) "Average Ambient Light"
-FROM fact_reading f 
+FROM fact_reading f
 JOIN dim_station s
-ON s.station_id = f.station_id 
+ON s.station_id = f.station_id
 AND s.isactive = 1
-AND f.time_id > (SYSDATE-1/24) 
+AND f.time_id > (SYSDATE-1/24)
 GROUP BY s.station_id;
 --------------------------------------------------------------------------------
 --Average ambientlight per active station for PAST DAY
 --------------------------------------------------------------------------------
 SELECT s.station_id "Station ID", ROUND(AVG(f.avg_ambientlight),2) "Average Ambient Light"
-FROM fact_reading f 
+FROM fact_reading f
 JOIN dim_station s
-ON s.station_id = f.station_id 
+ON s.station_id = f.station_id
 AND s.isactive = 1
-AND f.time_id > (SYSDATE-1) 
+AND f.time_id > (SYSDATE-1)
 GROUP BY s.station_id;
 --------------------------------------------------------------------------------
 --Average ambientlight per active station for PAST WEEK
 --------------------------------------------------------------------------------
 SELECT s.station_id "Station ID", ROUND(AVG(f.avg_ambientlight),2) "Average Ambient Light"
-FROM fact_reading f 
+FROM fact_reading f
 JOIN dim_station s
-ON s.station_id = f.station_id 
+ON s.station_id = f.station_id
 AND s.isactive = 1
-AND f.time_id > (SYSDATE-7) 
+AND f.time_id > (SYSDATE-7)
 GROUP BY s.station_id;
 --------------------------------------------------------------------------------
 --Average ambientlight per active station for PAST YEAR
@@ -330,7 +330,7 @@ GROUP BY s.station_id;
 SELECT s.station_id "Station ID", ROUND(AVG(f.avg_ambientlight),2) "Average Ambient Light"
 FROM fact_reading f
 JOIN dim_station s
-ON s.station_id = f.station_id 
+ON s.station_id = f.station_id
 AND s.isactive = 1
-AND f.time_id > (SYSDATE-365) 
+AND f.time_id > (SYSDATE-365)
 GROUP BY s.station_id;

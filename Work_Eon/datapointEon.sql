bf4967a791113234211c4428402c9f463b1319b1
Eon Viljoen <28807995@student.g.nwu.ac.za>
2019-10-02 15:35:47 +0200

    New air pressure datapoints

---
+++ Work_Eon/datapointEon.sql
@@ -0,0 +1,117 @@
+--------------------------------------------------------------------------------
+--#################### 1. AVERGAE AIR PRESSURE PER LOCATION #####################
+--------------------------------------------------------------------------------
+-- average airpressure per lcoation per hour
+SELECT
+    T.HOUR AS "Hour of past day",
+    L.LOCATION_NAME AS "Location Name",
+    AVG(F.AVG_AIRPRESSURE) AS "AVG Air Pressure"
+FROM FACT_READING F JOIN DIM_LOCATION L
+ON L.LOCATION_ID=F.LOCATION_ID
+JOIN DIM_TIME T
+ON F.TIME_ID=T.TIME_ID AND T.TIME_ID > SYSDATE-1
+GROUP by T.HOUR, L.LOCATION_NAME;
+--------------------------------------------------------------------------------
+-- average airpressure per lcoation per week
+SELECT
+    T.DAY AS "Days of previous week",
+    L.LOCATION_NAME AS "Location Name",
+    AVG(f.avg_airpressure) AS "AVG Air Pressure"
+FROM FACT_READING F JOIN DIM_LOCATION L
+ON L.LOCATION_ID=F.LOCATION_ID
+JOIN DIM_TIME T
+ON F.TIME_ID=T.TIME_ID AND T.TIME_ID > SYSDATE-7
+GROUP by T.DAY, L.LOCATION_NAME;
+--------------------------------------------------------------------------------
+-- average airpressure per lcoation per month
+SELECT
+    T.DAY AS "Days of previous month",
+    L.LOCATION_NAME AS "Location Name",
+    AVG(F.avg_airpressure) AS "AVG Air Pressure"
+FROM FACT_READING F JOIN DIM_LOCATION L
+ON L.LOCATION_ID=F.LOCATION_ID
+JOIN DIM_TIME T
+ON F.TIME_ID=T.TIME_ID AND T.TIME_ID > SYSDATE-30
+GROUP by T.DAY, L.LOCATION_NAME;
+--------------------------------------------------------------------------------
+-- average airpressure per lcoation per year
+SELECT
+    T.MONTH AS "Months of previous year",
+    L.LOCATION_NAME AS "Location Name",
+    AVG(F.avg_airpressure) AS "AVG Air Pressure"
+FROM FACT_READING F JOIN DIM_LOCATION L
+ON L.LOCATION_ID=F.LOCATION_ID
+JOIN DIM_TIME T
+ON F.TIME_ID=T.TIME_ID AND T.TIME_ID > SYSDATE-365
+GROUP by T.MONTH, L.LOCATION_NAME;
+--------------------------------------------------------------------------------
+-- average airpressure per lcoation per all years
+SELECT
+    T.YEAR AS "Year",
+    L.LOCATION_NAME AS "Location Name",
+    AVG(F.avg_airpressure) AS "AVG Air Pressure"
+FROM FACT_READING F JOIN DIM_LOCATION L
+ON L.LOCATION_ID=F.LOCATION_ID
+JOIN DIM_TIME T
+ON F.TIME_ID=T.TIME_ID
+GROUP by T.YEAR, L.LOCATION_NAME;
+
+--------------------------------------------------------------------------------
+--#################### 2. AVERGAE AIR PRESSURE PER STATION ######################
+--------------------------------------------------------------------------------
+
+--Avergae airpressure per station per hour for last day
+SELECT
+    T.HOUR AS "Hours of past day",
+    S.STATION_ID AS "Station ID",
+    AVG(f.avg_airpressure) AS "AVG Air Pressure"
+FROM FACT_READING F JOIN DIM_STATION S
+ON S.STATION_ID=F.STATION_ID
+JOIN DIM_TIME T
+ON F.TIME_ID=T.TIME_ID AND T.TIME_ID > SYSDATE-1
+GROUP by T.HOUR, S.STATION_ID;
+--------------------------------------------------------------------------------
+--Avergae airpressure per station per day for last week
+SELECT
+    T.DAY AS "Day of past week",
+    S.STATION_ID AS "Station ID",
+    AVG(f.avg_airpressure) AS "AVG Air Pressure"
+FROM FACT_READING F JOIN DIM_STATION S
+ON S.STATION_ID=F.STATION_ID
+JOIN DIM_TIME T
+ON F.TIME_ID=T.TIME_ID AND T.TIME_ID > SYSDATE-7
+GROUP by T.DAY, S.STATION_ID;
+--------------------------------------------------------------------------------
+--Avergae airpressure per station per day for last month
+SELECT
+    T.DAY AS "Day of past month",
+    S.STATION_ID AS "Station ID",
+    AVG(F.avg_airpressure) AS "AVG Air Pressure"
+FROM FACT_READING F JOIN DIM_STATION S
+ON S.STATION_ID=F.STATION_ID
+JOIN DIM_TIME T
+ON F.TIME_ID=T.TIME_ID AND T.TIME_ID > SYSDATE-30
+GROUP by T.DAY, S.STATION_ID;
+--------------------------------------------------------------------------------
+--Avergae airpressure per station per month for last year
+SELECT
+    T.MONTH AS "Month of past year",
+    S.STATION_ID AS "Station ID",
+    AVG(F.avg_airpressure) AS "AVG Air Pressure"
+FROM FACT_READING F JOIN DIM_STATION S
+ON S.STATION_ID=F.STATION_ID
+JOIN DIM_TIME T
+ON F.TIME_ID=T.TIME_ID AND T.TIME_ID > SYSDATE-365
+GROUP by T.MONTH, S.STATION_ID;
+--------------------------------------------------------------------------------
+--Avergae airpressure per station per year for all time
+SELECT
+    T.YEAR AS "Year",
+    S.STATION_ID AS "Station ID",
+    AVG(F.avg_airpressure) AS "AVG Air Pressure"
+FROM FACT_READING F JOIN DIM_STATION S
+ON S.STATION_ID=F.STATION_ID
+JOIN DIM_TIME T
+ON F.TIME_ID=T.TIME_ID
+GROUP by T.YEAR, S.STATION_ID;
+--------------------------------------------------------------------------------
